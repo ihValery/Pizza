2c4a5ce8dc83f869c6732a9b194ede02f5dfd588
ihValery <ihValery@gmail.com>
2021-07-15 05:27:32 +0300

    Initial Commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Валерий Игнатьев on 14.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            HomeView()
+        }
+    }
+}
